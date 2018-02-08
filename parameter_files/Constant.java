da328a2dbe466aa128e91ecef99640d39e521899
ShalakaAigal <shalaka.aigal@gmail.com>
2018-02-08 10:03:21 -0800

    Deleting the unused other package pos.

--- parameter_files/Constant.java
+++ parameter_files/Constant.java
@@ -8,7 +8,7 @@ public class Constant {
 	
 	public  static final String PRODUCTS = "parameter_files/products.txt";
 	public  static final String TRANSACTIONS = "parameter_files/transaction.txt";
-	public  static final String STORENAME = "ABC STORE";
+	public  static final String STORENAME = "APPLE STORE";
 	public  static final String CASH = "CASH";
 	public  static final String CREDIT = "CREDIT";
 	public  static final String CHECK = "CHECK";
