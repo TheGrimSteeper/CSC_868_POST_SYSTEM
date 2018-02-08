38b4f6899c3ab2ae84716463bf5ecb286a746d57
ShalakaAigal <shalaka.aigal@gmail.com>
2018-02-08 00:21:05 -0800

    Committing ProductReader

--- pos/ProductReader.java
+++ pos/ProductReader.java
@@ -1,39 +1,48 @@
 package pos;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 import parameter_files.Constant;
 
+/**
+ * 
+ * @author shalaka
+ *Gets filename as an input and parses the file into productMap
+ */
 public class ProductReader {
 	
 	
-	public static HashMap<String, Product> parseProducts() {
+	ProductReader(String productFile){
 		
-		  HashMap<String, Product> productList = new HashMap<String, Product>();
+		parseProducts(productFile);
+		
+	}
+	
+	public static HashMap<String, Product> parseProducts(String productFile) {
+		
+		  HashMap<String, Product> productMap = new HashMap<String, Product>();
 
 	  try {
-		  File productFile = new File(Constant.PRODUCTS);
+		  File prodFile = new File(productFile);
 	 
-		  Scanner productScanner = new Scanner(productFile); 
+		  @SuppressWarnings("resource")
+		Scanner productScanner = new Scanner(prodFile); 
 	    	productScanner.useDelimiter("\\s{2,}");
 		    while(productScanner.hasNextLine()) {
 				Product product = new  Product();
 		    	product.setUPC(productScanner.next());
 		    	product.setProductDescription(productScanner.next());
 		    	product.setPrice(Double.parseDouble(productScanner.next()));  
-		    	productList.put(product.UPC, product);
-		    //System.out.println(product.UPC + " "+ product.productDescription);
+		    	productMap.put(product.UPC, product);
 	   } 
 	}
-	  catch(Exception e){
-		  
+	  catch(FileNotFoundException ex){
+		  System.out.print("Products file is not found" + ex);
 	  }
-	return productList;
+	return productMap;
 	  }
 	
 }
-
-
-
