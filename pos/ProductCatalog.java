9aa6fb9b6a73c72ad9d3f9b214f6da78ab60fc5e
nikko <anjb0588@gmail.com>
2018-02-08 01:33:03 -0800

    rewrote ProductCatalog.java

--- pos/ProductCatalog.java
+++ pos/ProductCatalog.java
@@ -1,18 +1,43 @@
 package pos;
 
-public class ProductCatalog {
-	Products[] products;
-	
-	ProductCatalog(Products[] products){
-		this.products = products;
-	}
-	
-	public String toString() {
-		String string = "";
-		for(int i = 0; i < products.length; i++) {
-			string += this.products[i].UPC + " " + this.products[i].product + " " + this.products[i].price + "\n";
-		}
-		return string;
-	}
+import java.util.*;
+import java.io.File;
 
-}
+import parameter_files.Constant;
+
+public class ProductCatalog {
+	
+	private HashMap<String, Product> productList = new HashMap<String, Product>();
+	
+	ProductCatalog(HashMap<String, Product> productList){
+	   this.productList = productList;
+	}
+	
+
+	public Product lookupProduct(String UPC){
+	   Product value = productList.get(UPC);
+	   if(value == null){
+	   System.out.println("UPC NOT FOUND");
+	}
+	   System.out.println("product found with UPC" + UPC + " = " + value );
+		
+	   return value;
+	}
+	
+	 public void addItem(String UPC, Product product){
+		 
+		productList.put(UPC, product);
+		
+	 }
+	 /*
+	 public void print(HashMap<String, Product> productList){
+		Set set = productList.entrySet();
+		Iterator it = set.iterator();
+		 while(it.hasNext()){
+		    Map.Entry mentry = (Map.Entry)it.next();
+		    System.out.println("UPC = " + mentry.getKey());
+		    System.out.println("Price = " + productList.get();
+	  }
+	 }
+	 */
+	}
